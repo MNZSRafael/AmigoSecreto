87b8d73f91000faf36dfe2922c327e93ac42a913
MNZSRafael <135064113+MNZSRafael@users.noreply.github.com>
2023-11-27 15:04:46 -0300

    versão final 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+let amigos = []
+
 function adicionar ()
 {
     let amigo = document.getElementById("nome-amigo");
@@ -13,4 +16,40 @@ function adicionar ()
     listaDeAmigos.textContent = listaDeAmigos.textContent + `, ` + amigo.value;
     }
 
+    amigos.push(amigo.value);
+    amigo.value = " ";
+
+}
+
+function sortear()
+{
+    embaralhar(amigos);
+    let sorteio = document.getElementById("lista-sorteio");
+    for (let i = 0; i < amigos.length; i++) 
+    { 
+        if (i == amigos.length - 1)
+        {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + `--> `+ amigos[0] + `<br/>`;
+        }
+        else
+        {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i]+ `--> `+ amigos[i+1] + `<br/>`;
+        }
+    
+
+    }
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar()
+{
+    amigos = [];
+    document.getElementById ("lista-amigos").innerHTML = " ";
+    document.getElementById("lista-sorteio").innerHTML = " ";
 }
